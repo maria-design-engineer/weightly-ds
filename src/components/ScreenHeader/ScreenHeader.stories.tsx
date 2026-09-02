be2a36fd66f4716179bf3a60511e0d288f485d2a
Maria Abramovitch <Abramovitch-m@yandex.ru>
2026-09-03 01:04:10 +0300

    Шапка экрана: кнопка и дата прижаты к разным краям, ширина экрана 328

--- src/components/ScreenHeader/ScreenHeader.stories.tsx
+++ src/components/ScreenHeader/ScreenHeader.stories.tsx
@@ -5,6 +5,13 @@ import { ScreenHeader } from './ScreenHeader'
 const meta = {
   title: 'Custom/ScreenHeader',
   component: ScreenHeader,
+  decorators: [
+    (Story) => (
+      <div style={{ width: 328 }}>
+        <Story />
+      </div>
+    ),
+  ],
   argTypes: { content: { control: 'text' }, onBack: { control: false } },
   args: { content: '12 мая, понедельник' },
 } satisfies Meta<typeof ScreenHeader>
